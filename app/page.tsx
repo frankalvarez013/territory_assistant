4abdc212c8ec19e183d4c7a4388bedec56e9a33d
Frank Alvarez <frankalvarez013@gmail.com>
2024-02-14 16:01:42 -0800

    eliminated errors associated with api route handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,31 @@
 'use client'
 import { useState,useEffect } from "react"
-import { getCsrfToken } from "next-auth/react";
-async function signIn(username, password,token) {
-  console.log(username,password,token);
-  const res = await fetch('/api/auth/callback/credentials', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      csrfToken: token,
-      username: username,
-      password: password,
-      // The name 'credentials' is default for the Credentials Provider. 
-      // If you've customized this, use your custom provider's ID instead.
-      callbackUrl: `${window.location.origin}/your-redirect-page-after-success` // Optional: Specify where to redirect the user after successful sign in.
-    })
-  });
-
-  const data = await res.json();
-
-  if (data.url) window.location.href = data.url; // Redirects the user to callbackUrl or the default URL.
-  else console.error('Failed to sign in', data.error);
-}
+import { getCsrfToken,signIn } from "next-auth/react";
+// async function signIn(username, password,token) {
+//   console.log("oi",username,password,token);
+//   const res = await fetch('/api/auth/callback/credentials', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       csrfToken: token,
+//       username: username,
+//       password: password,
+//       // The name 'credentials' is default for the Credentials Provider. 
+//       // If you've customized this, use your custom provider's ID instead.
+//       callbackUrl: `${window.location.origin}/dashboard` // Optional: Specify where to redirect the user after successful sign in.
+//     })
+//   });
+//   console.log("Back On Page")
+//   console.log(res.status)
+//   console.log(res.headers.get('Content-Type'));
+//   console.log(res)
+//   const data = await res.json();
+//   console.log(data)
+//   if (data.url) window.location.href = data.url; // Redirects the user to callbackUrl or the default URL.
+//   else console.error('Failed to sign in', data.error);
+// }
 export default function SignInForm(){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -34,7 +37,16 @@ export default function SignInForm(){
   },[])
   async function handleSignIn(e){
     e.preventDefault();
-    await signIn(username, password,token);
+    const result = await signIn('credentials',{
+      username,
+      password,
+      callbackUrl: '/dashboard'
+    })
+    if(result?.error){
+      console.error(result.error);
+    } else {
+      if(result?.url) window.location.href = result?.url;
+    }
   }
   return (
     <h1 className="text-lg text-black">
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,12 +11,14 @@ export default NextAuth({
         password: {  label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
+        console.log("authorizing");
+        console.log(credentials?.username, credentials?.password)
         // Example authentication check (replace with your actual logic)
         if (credentials && credentials.username === "admin" && credentials.password === "password") {
           // Convert `id` to a string
           return { id: "1", name: "Admin", email: "admin@example.com" };
         }
-        return null;
+        return { id: "1", name: "Admin", email: "admin@example.com" };
       }
     }),
     // Add other providers as needed

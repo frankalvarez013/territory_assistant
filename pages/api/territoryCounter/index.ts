27875e1b6bed0d961c5a81368fd5519a9bfca6d7
Frank Alvarez <frankalvarez013@gmail.com>
2024-02-15 17:21:59 -0800

    Revert "fixed API resolved without sending a response for /api/user, this may result in stalled requests. issue for each api route"
    This reverts commit cbf30e94e84177773c2a4ef3e05583eb83636f6c.

---
+++ pages/api/territoryCounter/index.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from '@/prisma/client';
+import type { TerritoryCounter } from "@prisma/client";
+export async function GET(request:NextApiRequest, response:NextApiResponse){
+    const congregationID = Array.isArray(request.query.congId) ? request.query.congId[0] : request.query.congId;
+    let getCounter: TerritoryCounter | TerritoryCounter[] | null;
+    if(congregationID){
+        try{
+            getCounter = await prisma.territoryCounter.findMany({
+                where: {
+                    congregationID: congregationID,
+                }
+            });
+        } catch(e) {
+            return response.status(404).json({message: `territoryCounter GET Transaction failed:\n ${e}`});
+        }
+    } else {
+        getCounter = await prisma.territoryCounter.findMany({});
+    }
+    if (!getCounter){
+        return response.status(404).json({message: "TerritoryCounter Record not found"});
+    }
+    return response.status(201).json(getCounter);
+}
+
+export async function DELETE(request:NextApiRequest, response:NextApiResponse){
+    const congregationID = Array.isArray(request.query.congId) ? request.query.congId[0] : request.query.congId;
+    let getCounter: TerritoryCounter | null = null;
+    if(congregationID){
+        try{
+            getCounter = await prisma.territoryCounter.delete({
+                where: {
+                    congregationID: congregationID,
+                }
+            })
+        }catch(e) {
+            return response.status(404).json({message: `territoryCounter DELETE Transaction failed:\n ${e}`});
+        }
+        
+    }
+    if (!getCounter){
+        return response.status(404).json({message: "TerritoryCounter Record not found"});
+    }
+    return response.status(201).json(getCounter);
+}
+
+export default async function handler(req:NextApiRequest,res:NextApiResponse){
+    switch(req.method){
+        case 'GET':
+            GET(req,res);
+        case 'DELETE':
+            DELETE(req,res);
+    }
+}
